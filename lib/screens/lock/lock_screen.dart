2fe51f2f3164e347f6f38da7a35d31d61f13ff63
Florian326 <61353423+Florian326@users.noreply.github.com>
2021-03-02 14:18:18 +0100

    angepasst wie im stakeScreen

--- lib/screens/lock/lock_screen.dart
+++ lib/screens/lock/lock_screen.dart
@@ -1,22 +1,23 @@
 import 'dart:ui';
 
 import 'package:deus/core/widgets/back_button.dart';
+import 'package:deus/core/widgets/cross_fade_button.dart';
 import 'package:deus/core/widgets/dark_button.dart';
 import 'package:deus/core/widgets/filled_gradient_selection_button.dart';
 import 'package:deus/core/widgets/header_with_address.dart';
 import 'package:deus/core/widgets/selection_button.dart';
 import 'package:deus/core/widgets/text_field_with_max.dart';
+import 'package:deus/core/widgets/toast.dart';
 import 'package:deus/statics/my_colors.dart';
 import 'package:deus/statics/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
 
-enum LockStates {
+enum ButtonStates {
   hasToApprove,
-  pendingApprove,
-  canLock,
-  lockPendingApprove,
-  lockApproved
+  pendingApproveDividedButton,
+  isApproved,
+  pendingApproveMergedButton
 }
 
 class LockScreen extends StatefulWidget {
@@ -29,17 +30,20 @@ class LockScreen extends StatefulWidget {
 class _LockScreenState extends State<LockScreen> {
   final String address = '0x3a6dabD6B5C75291a3258C29B418f5805792a87e';
 
-
   final balance = 13.234;
 
   final gradient = MyColors.blueToGreenGradient;
 
-  LockStates _lockState = LockStates.hasToApprove;
+  ButtonStates _lockState = ButtonStates.hasToApprove;
   bool _showToast = false;
 
   final _textController = TextEditingController();
+                                                //Date Format: year, month, day, hour, minute, seconds
+  int endTime = DateTime.now().millisecondsSinceEpoch + DateTime(2021, 5,3, 10, 0, 0).difference(DateTime.now()).inMilliseconds; // Time until countDown ends
 
-  int endTime = DateTime.now().millisecondsSinceEpoch + 1000 * 30; // current Time in the locking period
+  static const kBigSpacer = SizedBox(height: 20);
+  static const kMediumSpacer = SizedBox(height: 15);
+  static const kSmallSpacer = SizedBox(height: 12);
 
   @override
   Widget build(BuildContext context) {
@@ -57,47 +61,25 @@ class _LockScreenState extends State<LockScreen> {
                 HeaderWithAddress(
                   walletAddress: address,
                 ),
-                SizedBox(
-                  height: 23,
-                ),
+                kBigSpacer,
                 BackButtonWithText(),
-                SizedBox(
-                  height: 20,
-                ),
+                kBigSpacer,
                 Text(
                   'Lock your DEA',
                   style: TextStyle(fontSize: 25),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
+                kBigSpacer,
                 Text(
                   'If you lock your DEA you will mint sDEA which can be used for single asset staking or for liquidity pools.',
                   style: MyStyles.whiteMediumTextStyle,
                 ),
-                CountdownTimer(
-                  endTime: endTime,
-                  widgetBuilder: (_, time) {
-                    if (time == null) {
-                      return Text('Current locking period still lasts 0 Days, 0 Hours, 0 Minutes and 0 Seconds.', style: MyStyles.gradientMediumTextStyle);
-                    } else {
-                      return Text(
-                        'Current locking period still lasts ${time.days != null ? time.days : 0} Days, ${time.hours != null ? time.hours : 0} Hours, ${time.min != null ? time.min : 0} Minutes and ${time.sec != null ? time.sec : 0} Seconds.',
-                        style: MyStyles.gradientMediumTextStyle,
-                      );
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
+                _buildCountDownTimer(),
+                kSmallSpacer,
                 Text(
                   'You will also receive TIME tokens that can be used for single asset staking. Read more about the use of TIME token on our wiki',
                   style: MyStyles.whiteMediumTextStyle,
                 ), //TODO: add link
-                SizedBox(
-                  height: 20,
-                ),
+                kBigSpacer,
                 Padding(
                   padding: const EdgeInsets.only(left: 15),
                   child: Text(
@@ -105,32 +87,25 @@ class _LockScreenState extends State<LockScreen> {
                     style: MyStyles.lightWhiteSmallTextStyle,
                   ),
                 ),
-                SizedBox(
-                  height: 5,
-                ),
+                SizedBox(height: 5),
                 TextFieldWithMax(
                   controller: _textController,
                   maxValue: balance,
                 ),
-                SizedBox(
-                  height: 12,
-                ),
+                kSmallSpacer,
                 DarkButton(
                   label: 'Show me the contract',
                   onPressed: () {},
                   labelStyle: MyStyles.whiteMediumTextStyle,
                 ),
-                SizedBox(
-                  height: 12,
-                ),
+                kSmallSpacer,
                 _buildLockApproveButton(),
-                SizedBox(
-                  height: 15,
-                ),
-                if (_lockState == LockStates.hasToApprove) _buildSteps(),
+                kMediumSpacer,
+                if (_lockState == ButtonStates.hasToApprove) _buildSteps(),
                 Spacer(),
-                if ((_lockState != LockStates.hasToApprove && _showToast) ||
-                    (_lockState == LockStates.lockPendingApprove && _showToast))
+                if ((_lockState != ButtonStates.hasToApprove && _showToast) ||
+                    (_lockState == ButtonStates.pendingApproveDividedButton &&
+                        _showToast))
                   _buildToast()
               ],
             ),
@@ -140,53 +115,56 @@ class _LockScreenState extends State<LockScreen> {
     );
   }
 
-  Container _buildToast() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(MyStyles.cardRadiusSize),
-          color: _lockState != LockStates.pendingApprove &&
-                  _lockState != LockStates.lockPendingApprove
-              ? Color(MyColors.ToastGreen)
-              : Color(MyColors.ToastGrey)),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                _lockState != LockStates.pendingApprove &&
-                        _lockState != LockStates.lockPendingApprove
-                    ? 'Successful'
-                    : 'Transaction Pending',
-                style: MyStyles.whiteMediumTextStyle,
-              ),
-              Spacer(),
-              GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _showToast = false;
-                    });
-                  },
-                  child: Icon(Icons.close))
-            ],
-          ),
-          GestureDetector(
-            onTap: () {}, //TODO: open link
-            child: Row(
-              children: [
-                Text(
-                  'Approved sDEA spend',
-                  style: MyStyles.whiteMediumUnderlinedTextStyle,
-                ),
-                Transform.rotate(
-                  angle: 150,
-                  child: Icon(Icons.arrow_right_alt_outlined),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+  CountdownTimer _buildCountDownTimer() {
+    return CountdownTimer(
+      endTime: endTime,
+      widgetBuilder: (_, time) {
+        if (time == null) {
+          return Text(
+              'Current locking period still lasts 0 Days, 0 Hours, 0 Minutes and 0 Seconds.',
+              style: MyStyles.gradientMediumTextStyle);
+        } else {
+          return Text(
+            'Current locking period still lasts ${time?.days ?? 0} Days, ${time?.hours ?? 0} Hours, ${time?.min ?? 0} Minutes and ${time?.sec ?? 0} Seconds.',
+            style: MyStyles.gradientMediumTextStyle,
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildToast() {
+    if (_lockState != ButtonStates.pendingApproveDividedButton &&
+        _lockState != ButtonStates.pendingApproveMergedButton) {
+      return _buildTransactionSuccessToast();
+    } else {
+      return _buildTransactionPending();
+    }
+  }
+
+  Toast _buildTransactionSuccessToast() {
+    return Toast(
+      label: 'Successful',
+      color: Color(MyColors.ToastGreen),
+      onPressed: () {},
+      onClosed: () {
+        setState(() {
+          _showToast = false;
+        });
+      },
+    );
+  }
+
+  Toast _buildTransactionPending() {
+    return Toast(
+      label: 'Transaction Pending',
+      color: Color(MyColors.ToastGrey),
+      onPressed: () {},
+      onClosed: () {
+        setState(() {
+          _showToast = false;
+        });
+      },
     );
   }
 
@@ -222,81 +200,42 @@ class _LockScreenState extends State<LockScreen> {
   }
 
   Widget _buildLockApproveButton() {
-    return AnimatedCrossFade(
-        firstChild: Row(
-          children: [
-            Expanded(
-              child: FilledGradientSelectionButton(
-                onPressed: () {
-                  setState(() {
-                    _lockState = LockStates.pendingApprove;
-                    _showToast = true;
-                  });
-                  Future.delayed(Duration(seconds: 3), () {
-                    setState(() {
-                      _lockState = LockStates.canLock;
-                    });
-                  });
-                },
-                label: 'APPROVE',
-                textStyle: MyStyles.blackMediumTextStyle,
-                gradient: gradient,
-              ),
-            ),
-            if (_lockState != LockStates.canLock)
-              SizedBox(
-                width: 15,
-              ),
-            Expanded(
-              child: SizedBox(
-                child: SelectionButton(
-                  gradient: gradient,
-                  child: _lockState == LockStates.pendingApprove
-                      ? SizedBox(
-                          height: 20,
-                          width: 20,
-                          child: CircularProgressIndicator(
-                            valueColor:
-                                AlwaysStoppedAnimation<Color>(Colors.white),
-                            strokeWidth: 2,
-                          ),
-                        )
-                      : null,
-                  onPressed: (bool) {},
-                  label: 'LOCK',
-                  selected: false,
-                  textStyle: _lockState == LockStates.canLock
-                      ? MyStyles.blackMediumTextStyle
-                      : MyStyles.lightWhiteMediumTextStyle,
-                ),
-              ),
-            )
-          ],
-        ),
-        secondChild: SizedBox(
-          width: double.infinity,
-          child: SelectionButton(
-              gradient: gradient,
-              onPressed: (bool) {
-                setState(() {
-                  _lockState = LockStates.lockPendingApprove;
-                  _showToast = true;
-                  Future.delayed(Duration(seconds: 3), () {
-                    setState(() {
-                      _lockState = LockStates.lockApproved;
-                    });
-                  });
-                });
-              },
-              label: 'LOCK',
-              selected: true,
-              textStyle: MyStyles.blackMediumTextStyle),
-        ),
-        crossFadeState: _lockState == LockStates.canLock ||
-                _lockState == LockStates.lockPendingApprove ||
-                _lockState == LockStates.lockApproved
-            ? CrossFadeState.showSecond
-            : CrossFadeState.showFirst,
-        duration: Duration(milliseconds: 150));
+    return CrossFadeButton(
+      gradientButtonLabel: 'APPROVE',
+      mergedButtonLabel: 'LOCK',
+      offButtonLabel: 'LOCK',
+      showBothButtons: _lockState == ButtonStates.hasToApprove ||
+          _lockState == ButtonStates.pendingApproveDividedButton,
+      showLoading: _lockState == ButtonStates.pendingApproveDividedButton ||
+          _lockState == ButtonStates.pendingApproveMergedButton,
+      onPressed: () async {
+        if (_lockState == ButtonStates.isApproved ||
+            _lockState == ButtonStates.pendingApproveMergedButton) {
+          setState(() {
+            _showToast = true;
+            _lockState = ButtonStates.pendingApproveMergedButton;
+          });
+          await Future.delayed(Duration(seconds: 3));
+          setState(() {
+            if (!_showToast) {
+              _showToast = true;
+            }
+            _lockState = ButtonStates.isApproved;
+          });
+        } else {
+          setState(() {
+            _lockState = ButtonStates.pendingApproveDividedButton;
+            _showToast = true;
+          });
+          await Future.delayed(Duration(seconds: 3));
+          setState(() {
+            if (!_showToast) {
+              _showToast = true;
+            }
+            _lockState = ButtonStates.isApproved;
+          });
+        }
+      },
+    );
   }
 }
