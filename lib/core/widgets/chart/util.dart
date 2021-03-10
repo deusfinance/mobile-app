0de54c40a62fbefba7be5ca783165f900d307b06
Florian326 <61353423+Florian326@users.noreply.github.com>
2021-03-10 13:28:48 +0100

    duration --> timeSpan in extra file

---
+++ lib/core/widgets/chart/util.dart
@@ -0,0 +1,7 @@
+Map<Duration, String> durationTimeSpan = {
+  Duration(hours: 1): 'Past Hour',
+  Duration(days: 1): 'Past Day',
+  Duration(days: 7): 'Past Week',
+  Duration(days: 30): 'Past Month',
+  Duration(days: 365): 'Past Year',
+};
