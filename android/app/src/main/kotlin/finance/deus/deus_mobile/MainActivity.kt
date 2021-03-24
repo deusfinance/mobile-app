ea25d7c7d5794ad327c59bd0b0a759900291e60b
Kazem <kazemgh.hookman@gmail.com>
2021-03-24 11:44:26 +0430

    version increased

--- android/app/src/main/kotlin/finance/deus/deus_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/finance/deus/deus_mobile/MainActivity.kt
@@ -4,3 +4,15 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+
+
+
+//import androidx.annotation.NonNull
+//import io.flutter.embedding.android.FlutterFragmentActivity
+//import io.flutter.embedding.engine.FlutterEngine
+//import io.flutter.plugins.GeneratedPluginRegistrant
+//class MainActivity: FlutterFragmentActivity() {
+//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine)
+//    }
+//}
